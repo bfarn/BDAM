ce5f8b8b9e70263b8209ceec6c93b7d1f6aaec72
bfarn <github.com@bfarn.com>
2016-07-12 12:38:52 -0700

    Basic functionality in place
    Still needs to refresh template listing when changing projects via
    dropdown. Also it's still ugly.

--- TemplateBrowser/templateBrowser.js
+++ TemplateBrowser/templateBrowser.js
@@ -2,16 +2,21 @@
 var fs = require('fs');
 var npath = require('path');
 var async = require('async');
+var csInterface;
 
 //Directory building stuff
 var ignoreProjectsList = [".DS_Store", "_COPYdonotdelete", "_COPYdonotdelete copy", "_DO_NOT_USE", "Work"];
+var ignoreFilesList = [".DS_Store"];
 var isWin = /^win/.test(process.platform);
 var workRoot = process.env.BUCK_WORK_ROOT;
 var currentProject = process.env.BUCK_PROJECT;
 var activeProjectIndex = 0;
+var templateRoot = workRoot + "\\" + currentProject + "\\Ref\\templates";
 if(!isWin) { 
 	workRoot = "/Volumes/Work/current";
+	templateRoot = workRoot + "/" + currentProject + "/Ref/templates";
 }
+console.log(templateRoot);
 
 
 
@@ -61,13 +66,7 @@ function buildPanel() {
 	templateBox.append(templateHeading);
 	
 	//Build template list
-	var templateList = $(document.createElement("ul")).attr("id", "templateList");
-	for(var i=1; i < 21; i++){
-		var testVal = $(document.createElement("li")).attr("class", "templateListItems");
-		testVal.append(i);
-		templateList.append(testVal);
-	}
-	templateBox.append(templateList);
+	templateBox.append(buildTemplateList());
 	
 	
 	
@@ -107,7 +106,7 @@ function buildProjectList() {
 	//Check for the work dir
 	if(!fs.existsSync(workRoot)){
 		console.log("Folder not found!");
-		activeProjects = ["--Projects directory not found--"]
+		activeProjects = ["--Projects directory not found--"];
 		return activeProjects;
 	}
 	
@@ -118,14 +117,75 @@ function buildProjectList() {
 	return activeProjects;
 }
 
+function buildTemplateList() {
+	//Initialize blank Unordered List
+	var templateList = $(document.createElement("ul")).attr("id", "templateList");
+	
+	//Populate list with filtered dir contents
+	var templateDirContents = [];
+	if(!fs.existsSync(templateRoot)){
+		console.log("No templates found!");
+		var listItem = $(document.createElement("li")).attr("class", "templateListItems");
+		listItem.append("No templates found");
+		templateList.append(listItem);
+		return templateList;
+	}
+	
+	var cleanTemplateDirContents = cleanArray(fs.readdirSync(templateRoot), ignoreFilesList);
+	
+	for(var i=0; i < cleanTemplateDirContents.length; i++){
+		//Create empty list item
+		var listItem = $(document.createElement("li")).attr("class", "templateListItems");
+		
+		//Create empty link to add to list item
+		var clickableListItem = $(document.createElement("a")).attr("class", "templateListItems");
+		
+		//Add template name to link
+		clickableListItem.text(cleanTemplateDirContents[i]);
+		
+		//Add file path to link
+		//clickableListItem.attr("filePath", (templateRoot + "\\" + cleanTemplateDirContents[i] ));
+		var tempFilePath = templateRoot + "/" + cleanTemplateDirContents[i];
+		tempFilePath = slash(tempFilePath);
+		var onClickScript = "openTest('" + tempFilePath + "')";
+		console.log(onClickScript);
+		clickableListItem.attr("onclick", onClickScript);
+		
+		//Add link to list item
+		listItem.append(clickableListItem);
+		
+		//Add list item to list
+		templateList.append(listItem);
+	}
+	return templateList;
+}
 
+function slash(str) {
+	console.log("slash(" + str + ")");
+	var isExtendedLengthPath = /^\\\\\?\\/.test(str);
+	var hasNonAscii = /[^\x00-\x80]+/.test(str);
+	if (isExtendedLengthPath || hasNonAscii) {
+		return str;
+	}
+	return str.replace(/\\/g, '/');
+};
 
 //***************************************************************************************************************************
 //* High Level
 //***************************************************************************************************************************
 function onLoaded() {
+	csInterface = new CSInterface();
 	buildPanel();
 	console.log("Template Browser ready");
 }
 
-
+function openTest(openPath) {
+	console.log("Opening a template");
+	openScript = "tempFilePath = '" + openPath + "'; tempFile = new File(tempFilePath); app.open(tempFile);"
+	console.log(openScript);
+	csInterface.evalScript(openScript);
+	dupeScript = "var templateSource = app.activeDocument; templateSource.duplicate(); templateSource.close(SaveOptions.DONOTSAVECHANGES);"
+	console.log(openScript);
+	csInterface.evalScript(dupeScript);
+	
+}
